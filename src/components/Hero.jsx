4a96413f834b91e16a6c3fe66f31df04bc417d34
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-01 15:32:22 -0600

    added hero and desc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,14 @@ const Hero = () => {
           GitHub
         </button>
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAT GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   )
 }
