2cf119c07f33a1fd054847e0d341d27297ecc8fc
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-03 16:23:31 -0600

    get url alert

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,20 +5,28 @@ import { copy, linkIcon, loader, tick } from "../assets";
 
 
 const Demo = () => {
+const [article, setArticle] = useState({
+  url: "",
+  summary: "",
+})
+
+const handleSubmit = async (e) => {
+  alert("Submitted")
+}
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form className="relative flex justify-center items-center"
-        onSubmit={() => {}}>
+        onSubmit={handleSubmit}>
           <img src={linkIcon}
           alt="link
           _icon"
           className='absolute left-0 my-2 ml-3 w-5' />
           <input type="url"
           placeholder='Enter a URL'
-          value=""
-          onChange={() => {}}
+          value={article.url}
+          onChange={(e) => {setArticle({...article, url:e.target.value})}}
           required
           className='url_input peer' />
           <button
