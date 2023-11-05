bd28c45566adff55012e7681bf5ad8a3f247270d
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-04 18:09:50 -0600

    edit main

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App.jsx'
 import {store} from './services/store.js'
 
+// Main Page for AI Summarizer
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
